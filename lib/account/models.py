7d06c2a7a71b980a478a32ed48f6c8d22172fd38
bitwopi <donmormajor@gmail.com>
2022-11-08 15:19:35 +0300

    start of the project

---
+++ lib/account/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Group
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, first_name, second_name, patronymic=None, password=None):
+        """
+        Creates and saves a User with the given email, first name, second name, patronymic and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        if not first_name:
+            raise ValueError('Users must have a first name')
+
+        if not second_name:
+            raise ValueError('Users must have a second name')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            second_name=second_name,
+            patronymic=patronymic,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class CustomUser(AbstractBaseUser):
+    # DATABASE FIELDS
+    email = models.EmailField(unique=True, primary_key=True)
+    first_name = models.CharField(max_length=50, verbose_name="Имя", null=False)
+    second_name = models.CharField(max_length=50, verbose_name="Фамилия", null=False)
+    patronymic = models.CharField(max_length=50, verbose_name="Отчество", blank=True, null=True)
+    group = models.ForeignKey(Group, on_delete=models.SET_NULL, default=None, null=True, blank=True,
+                              verbose_name="Группа")
+    date_joined = models.DateField(auto_now_add=True, verbose_name="Дата создания аккаунта")
+    avatar = models.ImageField(upload_to="users/avatars", null=True, blank=True)
+    # CONSTANTS
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [
+        'first_name',
+        'second_name',
+        'birth_date',
+    ]
+
+    # USER MANAGER
+    objects = CustomUserManager()
+
+    # META CLASS
+    class Meta:
+        ordering = ['second_name']
+
+    # TO STRING METHOD
+    def __str__(self):
+        return self.email
---
+++ lib/main_app/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from account.models import CustomUser
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50, verbose_name="Имя", null=False)
+    second_name = models.CharField(max_length=50, verbose_name="Фамилия", null=False)
+    patronymic = models.CharField(max_length=50, verbose_name="Отчество", null=True)
+    birth_date = models.DateField(verbose_name="Дата рождения", null=True)
+    description = models.TextField(verbose_name="Описание", null=True)
+    photo = models.ImageField(upload_to="authors/photos", null=True, blank=True)
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название", null=False)
+    description = models.TextField(verbose_name="Описание", null=True)
+    pages = models.IntegerField(verbose_name="Количество страниц", null=False)
+    cost = models.FloatField(verbose_name="Цена", null=False)
+    cover = models.ImageField(upload_to="books/covers", null=True, blank=True)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE, null=False)
+
+
+class Instance(models.Model):
+    book = models.ForeignKey('Book', on_delete=models.CASCADE, null=False)
+    in_stock = models.BooleanField(default=True, verbose_name="В наличии")
+
+
+class Rent(models.Model):
+    user_email = models.ForeignKey(CustomUser, models.CASCADE, null=False)
+    instance_id = models.ForeignKey("Instance", models.CASCADE, null=False)
+    start_date = models.DateField(verbose_name="Дата начала", null=False)
+    end_date = models.DateField(verbose_name="Дата окончания", null=False)
+    actual_end_date = models.DateField(verbose_name="Дата сдачи", null=True)
+    cost = models.FloatField(verbose_name="Индексация", null=True)
